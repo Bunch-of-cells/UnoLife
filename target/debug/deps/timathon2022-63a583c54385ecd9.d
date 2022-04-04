931fe5cba9dbce7029967cab9c2dc23e5a7f3e2e
Alumin112 <alumin112.welcome@gmail.com>
2022-04-04 21:43:40 +0530

    meh trying to gitignore target folder

--- target/debug/deps/timathon2022-63a583c54385ecd9.d
+++ target/debug/deps/timathon2022-63a583c54385ecd9.d
@@ -1,5 +1,5 @@
-/home/alumin112/Desktop/Timathon2022/timathon2022/target/debug/deps/timathon2022-63a583c54385ecd9.rmeta: src/main.rs
+/home/alumin112/Desktop/Timathon2022/target/debug/deps/timathon2022-63a583c54385ecd9.rmeta: src/main.rs
 
-/home/alumin112/Desktop/Timathon2022/timathon2022/target/debug/deps/timathon2022-63a583c54385ecd9.d: src/main.rs
+/home/alumin112/Desktop/Timathon2022/target/debug/deps/timathon2022-63a583c54385ecd9.d: src/main.rs
 
 src/main.rs:
--- target/debug/deps/timathon2022-a19ff61e632649ed.d
+++ target/debug/deps/timathon2022-a19ff61e632649ed.d
@@ -1,5 +1,5 @@
-/home/alumin112/Desktop/Timathon2022/timathon2022/target/debug/deps/timathon2022-a19ff61e632649ed.rmeta: src/main.rs
+/home/alumin112/Desktop/Timathon2022/target/debug/deps/timathon2022-a19ff61e632649ed.rmeta: src/main.rs
 
-/home/alumin112/Desktop/Timathon2022/timathon2022/target/debug/deps/timathon2022-a19ff61e632649ed.d: src/main.rs
+/home/alumin112/Desktop/Timathon2022/target/debug/deps/timathon2022-a19ff61e632649ed.d: src/main.rs
 
 src/main.rs:
