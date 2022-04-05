1befe62953f944ded3c073478dae088abbf62d01
Alumin112 <alumin112.welcome@gmail.com>
2022-04-05 17:17:24 +0530

    made Color struct useful

--- src/components/color.rs
+++ src/components/color.rs
@@ -1,7 +1,21 @@
-pub struct Color();
+pub struct Color {
+    red: f32,
+    green: f32,
+    blue: f32,
+    alpha: f32,
+}
 
 impl Color {
-    pub fn new(red: u8, green: u8, blue: u8, alpha: f32) -> [f32; 4] {
-        return [red as f32/255.0, green as f32/255.0, blue as f32/255.0, alpha];
+    pub fn new(red: u8, green: u8, blue: u8, alpha: f32) -> Self {
+        Color {
+            red: red as f32 / 255.0,
+            green: green as f32 / 255.0,
+            blue: blue as f32 / 255.0,
+            alpha,
+        }
+    }
+
+    pub fn as_array(&self) -> [f32; 4] {
+        [self.red, self.green, self.blue, self.alpha]
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -13,14 +13,18 @@ fn main() {
     let mut rng = rand::thread_rng();
     // types::Color
     let mut window: PistonWindow =
-        WindowSettings::new("Hello your computer has virus.", [640, 480]).build().unwrap();
+        WindowSettings::new("Hello your computer has virus.", [640, 480])
+            .build()
+            .unwrap();
     while let Some(event) = window.next() {
         window.draw_2d(&event, |context, graphics, _device| {
             clear([1.0; 4], graphics);
-            rectangle(Color::new(rng.gen::<u8>(), rng.gen::<u8>(), rng.gen::<u8>(), 1.0),
-                      [0.0, 0.0, 10000.0, 10000.0],
-                      context.transform,
-                      graphics);
+            rectangle(
+                Color::new(rng.gen::<u8>(), rng.gen::<u8>(), rng.gen::<u8>(), 1.0).as_array(),
+                [0.0, 0.0, 10000.0, 10000.0],
+                context.transform,
+                graphics,
+            );
         });
         sleep(Duration::from_millis(3));
     }
