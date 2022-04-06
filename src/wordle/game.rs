92b958c784d9151832d1b266f8293c22129bdf54
Alumin112 <alumin112.welcome@gmail.com>
2022-04-06 18:35:28 +0530

    wordle

--- src/wordle/game.rs
+++ src/wordle/game.rs
@@ -1,1 +1,82 @@
-pub struct Game {}
+use rand::prelude::IteratorRandom;
+use std::fs;
+
+pub struct Game<const GUESSES: usize = 6> {
+    word: String,
+    guesses: [Option<Guess>; GUESSES],
+    tries: usize,
+}
+
+impl<const GUESSES: usize> Game<GUESSES> {
+    pub fn new() -> Self {
+        let words = fs::read_to_string("wordle_list.txt").unwrap();
+        let word = words
+            .split_whitespace()
+            .choose(&mut rand::thread_rng())
+            .unwrap()
+            .to_string();
+        Self {
+            word,
+            guesses: [0; GUESSES].map(|_| None),
+            tries: 0,
+        }
+    }
+
+    pub fn guess(&mut self, guess: String) -> GuessResult {
+        if guess.len() != 5 {
+            panic!("The length of the guess was not 5 :: GUESS : {}", guess.len());
+        }
+        if guess.chars().any(|c| !c.is_alphabetic()) {
+            panic!("The guess was not full o alphabets : {}", guess);
+        }
+        if guess == self.word {
+            return GuessResult::Right;
+        }
+        if self.tries >= GUESSES {
+            return GuessResult::GameOver(&self.word);
+        }
+        self.tries += 1;
+        self.guesses[self.tries] = Some(Guess::new(guess, &self.word));
+        GuessResult::Wrong
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum GuessResult<'a> {
+    Right,
+    Wrong,
+    GameOver(&'a str),
+}
+
+#[derive(Debug, Clone)]
+pub struct Guess {
+    guess: String,
+    correct: u8,
+    out_of_order: u8,
+}
+
+impl PartialEq for Guess {
+    fn eq(&self, other: &Self) -> bool {
+        self.guess == other.guess
+    }
+}
+
+impl Guess {
+    fn new(guess: String, word: &str) -> Self {
+        let correct = guess
+            .chars()
+            .zip(word.chars())
+            .filter(|(i, v)| i == v)
+            .count() as u8;
+        let out_of_order = guess
+            .chars()
+            .zip(word.chars())
+            .filter(|(i, v)| i != v && word.contains(*i))
+            .count() as u8;
+        Self {
+            guess,
+            correct,
+            out_of_order,
+        }
+    }
+}
--- src/wordle/mod.rs
+++ src/wordle/mod.rs
@@ -1,1 +1,16 @@
+#![allow(dead_code)]
+
 pub mod game;
+pub use game::*;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[should_panic]
+    fn wordle_instance() {
+        let mut wordle = Game::<0>::new();
+        assert_eq!(wordle.guess("above".to_string()), GuessResult::Wrong);
+    }
+}
