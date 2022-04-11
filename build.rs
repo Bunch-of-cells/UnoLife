bb2892e0e6439a91d220d148c2a488edb4cf98d6
Alumin112 <alumin112.welcome@gmail.com>
2022-04-11 18:28:41 +0530

    changes

--- build.rs
+++ build.rs
@@ -5,8 +5,9 @@ fn main() {
         let assets = find_folder::Search::ParentsThenKids(3, 3)
             .for_folder("assets")
             .unwrap();
+        let path = assets.join("unolife_logo.ico");
         let mut res = WindowsResource::new();
-        res.set_icon(assets.join("unolife_logo.ico"));
+        res.set_icon(path.to_str().unwrap());
         res.compile().unwrap();
     }
 }
