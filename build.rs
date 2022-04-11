8278f5e02b1f4c9fa5d6e9792550cdea55a00e23
Alumin112 <alumin112.welcome@gmail.com>
2022-04-11 18:22:55 +0530

    fixes

--- build.rs
+++ build.rs
@@ -2,8 +2,11 @@ fn main() {
     #[cfg(windows)]
     {
         use winres::WindowsResource;
+        let assets = find_folder::Search::ParentsThenKids(3, 3)
+            .for_folder("assets")
+            .unwrap();
         let mut res = WindowsResource::new();
-        res.set_icon("../assets/unolife_logo.ico");
+        res.set_icon(assets.join("unolife_logo.ico"));
         res.compile().unwrap();
     }
 }
